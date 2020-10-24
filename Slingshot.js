af5f7371c332ab132c27440c92af8ae843626943
aashi2007 <aashi.ranawat@smspune.in>
2020-10-24 15:09:50 +0530

    added Catapult

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,10 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.s1=loadImage("sprites/sling1.png")
+        this.s2=loadImage("sprites/sling2.png")
+        this.s3=loadImage("sprites/sling3.png")
+
         World.add(world, this.sling);
     }
 
@@ -19,9 +23,23 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            
+            if(pointA.x<200)
+            {
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20,pointA.y,pointB.x+30,pointB.y);
+                
+            }
+            else
+            {
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25,pointA.y,pointB.x+30,pointB.y);
+            }
+            image(this.s1, 200, 20)
+            image(this.s2, 170,20)
+            image(this.s3, pointA.x-30, pointA.y-10, 15, 30)
+            strokeWeight(8);
+            stroke(84, 39, 15)
         }
-    }
-    
+    }    
 }
